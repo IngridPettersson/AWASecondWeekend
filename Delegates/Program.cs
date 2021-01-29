daadff05aedb050e38181f7ffd8931931e32dfa3
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-29 16:27:51 +0100

    Add project files.

---
+++ Delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A delegate is a type safe method pointer
+        }
+
+        static void WriteMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
