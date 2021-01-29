23bbc8d0a7f7f991bd785aaca6720e19f1ca1f7e
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-29 16:33:15 +0100

    Add changes

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -2,11 +2,15 @@
 
 namespace Delegates
 {
+    // This delegate, as it's written below, can point to ANY method that takes a string parameter and has a void return type.
+    // More generally described, the signature of a delegate must match the method in terms of parameters and return type.
+    public delegate void WriteMessageDelegate(string DelegateMessage);
+
     class Program
     {
         static void Main(string[] args)
         {
-            // A delegate is a type safe method pointer
+            // A delegate is a type safe method pointer.
         }
 
         static void WriteMessage(string message)
