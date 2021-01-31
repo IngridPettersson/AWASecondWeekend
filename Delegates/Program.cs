8b3587d34312d090e32d31ae8fa9a098273efbe8
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-31 20:45:53 +0100

    Add DelegatesUsage

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 
+// A delegate is a type safe method pointer
+
 namespace Delegates
 {
     // This delegate, as it's written below, can point to ANY method that takes a string parameter and has a void return type.
-    // More generally described, the signature of a delegate must match the method in terms of parameters and return type.
+    // More generally described, the signature of a delegate must match the method in terms of parameters and return type. This signature of the delegate also very much 
+    // resembles the signature of a method. (AND note that here we are declaring the delegate, even though it looks as if we were calling it. So in that sense, I might
+    // think of the delegate syntax almost as a variable syntax here.)
+
     public delegate void WriteMessageDelegate(string DelegateMessage);
+
 
     class Program
     {
         static void Main(string[] args)
         {
-            // A delegate is a type safe method pointer.
+
+            // To make the delegate point to a specific method you must create an instance of the delegate. This is how the delegate resembles a class.
+
+            WriteMessageDelegate del = new WriteMessageDelegate(WriteMessage); // As the argument you pass in the method that you want the delegate to point to.
+
+            // To invoke the method now all you have to do is to invoke the delegate and give it an argument that matches, which is a string in this case.
+
+            del("Hello from Delegate!");
+            del("I can call it again!");
+            del("And again and again! :)");
         }
 
         static void WriteMessage(string message)
---
+++ DelegatesUsage/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatesUsage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> empList = new List<Employee>()
+            {
+                new Employee {ID = 101, Name = "Madame", Salary = 5000, Experience = 5},
+                new Employee {ID = 101, Name = "Baba", Salary = 4000, Experience = 4},
+                new Employee {ID = 101, Name = "Strumpan", Salary = 6000, Experience = 6},
+                new Employee {ID = 101, Name = "Troj", Salary = 3000, Experience = 3}
+            };
+        }
+    }
+
+    class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+
+
+        public void PromoteEmployee(List<Employee> employeeList)
+        {
+            foreach (Employee employee in employeeList)
+            {
+                if (employee.Experience >= 5)
+                    Console.WriteLine($"{employee.Name} promoted");
+
+            }
+        }
+    }
+}
